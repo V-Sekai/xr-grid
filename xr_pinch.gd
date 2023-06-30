04c8e3f9641746c42d94214e72e293689675a4c5
moon man <humanimations10@gmail.com>
2023-06-30 19:14:14 +1000

    bugfix xr pinch

--- xr_pinch.gd
+++ xr_pinch.gd
@@ -8,8 +8,8 @@ var prev_hand_right_transform:Transform3D
 var prev_hand_left_pressed:bool
 var prev_hand_right_pressed:bool
 func _process(delta):
-	var hand_left_pressed = hand_left.is_button_pressed("grip")
-	var hand_right_pressed = hand_right.is_button_pressed("grip")
+	var hand_left_pressed = hand_left.is_button_pressed("grip_click")
+	var hand_right_pressed = hand_right.is_button_pressed("grip_click")
 	
 	if prev_hand_left_pressed && prev_hand_right_pressed:
 		transform = pinchTransform(
@@ -17,9 +17,9 @@ func _process(delta):
 			prev_hand_left_transform.origin, prev_hand_right_transform.origin,
 			hand_left.transform.origin, hand_right.transform.origin)
 	elif prev_hand_left_pressed:
-		transform = prev_hand_left_transform.affine_inverse() * hand_left.transform * transform
+		transform = hand_left.transform * prev_hand_left_transform.affine_inverse() * transform
 	elif prev_hand_right_pressed:
-		transform = prev_hand_right_transform.affine_inverse() * hand_right.transform * transform
+		transform = hand_right.transform * prev_hand_right_transform.affine_inverse() * transform
 	
 	prev_hand_left_transform = hand_left.transform
 	prev_hand_right_transform = hand_right.transform
@@ -30,11 +30,11 @@ func pinchTransform(_transform:Transform3D, from_a:Vector3, from_b:Vector3, to_a
 	from_b -= from_a
 	to_b -= to_a
 	
-	_transform = _transform.translated(to_a - from_a)
-	_transform = _transform.scaled(Vector3(1,1,1) * to_b.dot(to_b) / from_b.dot(from_b))
+	_transform = _transform.translated(-from_a).scaled(Vector3(1,1,1) * to_b.dot(to_b) / from_b.dot(from_b)).translated(to_a)
+	#_transform = _transform.translated(to_a - from_a)
 	
 	var axis = from_b.cross(to_b)
 	var angle = from_b.angle_to(to_b)
 	if axis: _transform = _transform.rotated(axis.normalized(), angle)
 	
-	return _transform.orthonormalized()
+	return _transform
