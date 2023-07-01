bd696cbee79c03172f10d829aa19c79f49307d53
celyk <50609684+celyk@users.noreply.github.com>
2023-07-02 01:21:20 +1000

    Add simple sketching

---
+++ SimpleSketcher/simple_sketch.gd
@@ -0,0 +1,44 @@
+class_name SimpleSketch extends RefCounted
+
+var target_mesh:ImmediateMesh
+
+func _init():
+	pass
+
+var prev_tangent:Vector3
+func addLine(from:Vector3, to:Vector3, from_size:float=.01, to_size:float=.01, from_color:Color=Color(0,0,0), to_color:Color=Color(0,0,0), begin_stroke:bool = false):
+	assert(target_mesh != null)
+	
+	var from_tangent = prev_tangent
+	var to_tangent = to - from
+	
+	if begin_stroke:
+		from_tangent = to_tangent
+	
+	# Begin draw.
+	target_mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
+	
+	target_mesh.surface_set_normal(from_tangent)
+	target_mesh.surface_set_uv(Vector2(0, -1))
+	target_mesh.surface_set_color(from_color)
+	target_mesh.surface_add_vertex(from)
+	
+	target_mesh.surface_set_normal(from_tangent)
+	target_mesh.surface_set_uv(Vector2(0, 1))
+	target_mesh.surface_set_color(from_color)
+	target_mesh.surface_add_vertex(from)
+	
+	target_mesh.surface_set_normal(to_tangent)
+	target_mesh.surface_set_uv(Vector2(0, -1))
+	target_mesh.surface_set_color(to_color)
+	target_mesh.surface_add_vertex(to)
+	
+	target_mesh.surface_set_normal(to_tangent)
+	target_mesh.surface_set_uv(Vector2(0, 1))
+	target_mesh.surface_set_color(to_color)
+	target_mesh.surface_add_vertex(to)
+	
+	# End drawing.
+	target_mesh.surface_end()
+	
+	prev_tangent = to_tangent
---
+++ strokes.gd
@@ -0,0 +1,38 @@
+extends MeshInstance3D
+
+@export var hand_left:XRController3D
+@export var hand_right:XRController3D
+
+@onready var simple_sketch = SimpleSketch.new()
+
+func _ready():
+	simple_sketch.target_mesh = mesh
+
+var prev_hand_left_transform:Transform3D
+var prev_hand_right_transform:Transform3D
+var prev_hand_left_pressed:bool
+var prev_hand_right_pressed:bool
+func _process(delta):
+	var hand_left_pressed = hand_left.is_button_pressed("grip_click")
+	var hand_right_pressed = hand_right.is_button_pressed("grip_click")
+	
+	if hand_left_pressed:
+		var from = to_local(prev_hand_left_transform.origin)
+		var to = to_local(hand_left.global_transform.origin)
+		
+		var hand_left_just_pressed = hand_left_pressed && !prev_hand_left_pressed
+
+		simple_sketch.addLine(from, to, .01, .01, Color(1,1,1), Color(1,1,1), hand_left_just_pressed)
+	
+	if hand_right_pressed:
+		var from = to_local(prev_hand_right_transform.origin)
+		var to = to_local(hand_right.global_transform.origin)
+		
+		var hand_right_just_pressed = hand_right_pressed && !prev_hand_right_pressed
+		
+		simple_sketch.addLine(from, to, .01, .01, Color(0,0,0), Color(0,0,0), hand_right_just_pressed)
+	
+	prev_hand_left_transform = hand_left.global_transform
+	prev_hand_right_transform = hand_right.global_transform
+	prev_hand_left_pressed = hand_left_pressed
+	prev_hand_right_pressed = hand_right_pressed
