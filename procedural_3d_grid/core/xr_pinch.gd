3981e4031bb20f642338072abac8fc6a330e645a
celyk <50609684+celyk@users.noreply.github.com>
2023-09-13 02:39:13 +1000

    Refactor navigation

--- procedural_3d_grid/core/xr_pinch.gd
+++ procedural_3d_grid/core/xr_pinch.gd
@@ -10,8 +10,8 @@ extends Node3D
 
 var prev_hand_left_transform: Transform3D
 var prev_hand_right_transform: Transform3D
-var prev_hand_left_pressed: float = 0
-var prev_hand_right_pressed: float = 0
+var prev_hand_left_grab: float = 0
+var prev_hand_right_grab: float = 0
 
 var _world_grab = WorldGrab.new()
 
@@ -19,35 +19,111 @@ var from_pivot : Vector3
 var to_pivot : Vector3
 var grab_pivot : Vector3
 var delta_transform: Transform3D
-var pinch_timer: float = 0.0
-const max_pinch_time: float = 0.1 # sensitivity?
 var target_transform: Transform3D = transform
-func _process(_delta: float) -> void:
-	var hand_left_pressed: float = hand_left.get_float("grip")
-	var hand_right_pressed: float = hand_right.get_float("grip")
 
+const max_pinch_time: float = 0.1 # sensitivity?
+
+enum Mode {
+	NONE, # One handed grab
+	GRAB, # One handed grab
+	PINCH, # Two handed grab
+	ORBIT, # Like spinning a globe
+}
+var state: Mode = Mode.NONE
+
+var left_hand_just_grabbed := BoolTimer.new()
+var right_hand_just_grabbed := BoolTimer.new()
+
+func _process(_delta: float) -> void:
+	var hand_left_grab: float = hand_left.get_float("grip")
+	var hand_right_grab: float = hand_right.get_float("grip")
+	
+	# Dampening
 	delta_transform = delta_transform.interpolate_with(Transform3D(), _delta*2.0)
-	if hand_left_pressed && hand_right_pressed && pinch_timer>0.0:
+	
+	# Always ctivate pinching if both hands are grabbing within max_pinch_timer
+	if hand_left_grab and not prev_hand_left_grab: left_hand_just_grabbed.set_true(max_pinch_time)
+	if hand_right_grab and not prev_hand_right_grab: right_hand_just_grabbed.set_true(max_pinch_time)
+	if left_hand_just_grabbed.value and right_hand_just_grabbed.value: state = Mode.PINCH
+	
+	# Always return to no grab if no hands are grabbing
+	if not (hand_left_grab or hand_right_grab): state = Mode.NONE
+	
+	match state:
+		Mode.NONE:
+			if not left_hand_just_grabbed && hand_left_grab:
+				state = Mode.GRAB
+			elif not right_hand_just_grabbed && hand_right_grab:
+				state = Mode.GRAB
+			
+		Mode.GRAB:
+			if hand_left_grab and hand_right_grab:
+				state = Mode.ORBIT
+			
+			if hand_left_grab:
+				from_pivot = prev_hand_left_transform.origin
+				to_pivot = prev_hand_left_transform.origin
+				
+				delta_transform = _world_grab.get_grab_transform(prev_hand_left_transform, hand_left.transform)
+			
+			if hand_right_grab:
+				from_pivot = prev_hand_right_transform.origin
+				to_pivot = prev_hand_right_transform.origin
+				
+				delta_transform = _world_grab.get_grab_transform(prev_hand_right_transform, hand_right.transform)
+			
+		Mode.PINCH:
+			if not (hand_left_grab and hand_right_grab):
+				state = Mode.GRAB
+			
+			from_pivot = (prev_hand_left_transform.origin + prev_hand_right_transform.origin)/2.0
+			to_pivot = (hand_left.transform.origin + hand_right.transform.origin)/2.0
+			
+			delta_transform = _world_grab.get_pinch_transform(prev_hand_left_transform.origin, prev_hand_right_transform.origin, hand_left.transform.origin, hand_right.transform.origin)
+			
+		Mode.ORBIT:
+			if not (hand_left_grab and hand_right_grab):
+				state = Mode.GRAB
+			
+			from_pivot = prev_hand_left_transform.origin
+			to_pivot = prev_hand_right_transform.origin
+			
+			delta_transform = _world_grab.get_orbit_transform(prev_hand_left_transform.origin, prev_hand_right_transform.origin, hand_left.transform.origin, hand_right.transform.origin)
+	
+	# Integrate motion
+	target_transform = delta_transform * target_transform
+	
+	# Smoothing
+	transform = _world_grab.split_blend(transform, target_transform, .3, .1, .1, transform * target_transform.affine_inverse() * to_pivot, to_pivot)
+	#transform = target_transform
+	
+	# Pass data required for the next frame
+	prev_hand_left_transform = hand_left.transform
+	prev_hand_right_transform = hand_right.transform
+	prev_hand_left_grab = hand_left_grab
+	prev_hand_right_grab = hand_right_grab
+
+'''delta_transform = delta_transform.interpolate_with(Transform3D(), _delta*2.0)
+	if hand_left_pressed && hand_right_pressed && both_hands_just_pressed.value:
 		from_pivot = (prev_hand_left_transform.origin + prev_hand_right_transform.origin)/2.0
 		to_pivot = (hand_left.transform.origin + hand_right.transform.origin)/2.0
 		
 		delta_transform = _world_grab.get_pinch_transform(prev_hand_left_transform.origin, prev_hand_right_transform.origin, hand_left.transform.origin, hand_right.transform.origin)
-	
-		pinch_timer = max_pinch_time
+
 	elif hand_left_pressed:
 		from_pivot = prev_hand_left_transform.origin
 		to_pivot = hand_left.transform.origin
 		
 		delta_transform = _world_grab.get_grab_transform(prev_hand_left_transform, hand_left.transform)
 		
-		if not prev_hand_left_pressed: pinch_timer = max_pinch_time
+		if not prev_hand_left_pressed: both_hands_just_pressed.set_true(max_pinch_time)
 	elif hand_right_pressed:
 		from_pivot = prev_hand_right_transform.origin
 		to_pivot = hand_right.transform.origin
 		
 		delta_transform = _world_grab.get_grab_transform(prev_hand_right_transform, hand_right.transform)
 		
-		if not prev_hand_right_pressed: pinch_timer = max_pinch_time
+		if not prev_hand_right_pressed: both_hands_just_pressed.set_true(max_pinch_time)
 	else:
 		#from_pivot = Vector3()
 		#to_pivot = Vector3()
@@ -57,29 +133,5 @@ func _process(_delta: float) -> void:
 	target_transform = delta_transform * target_transform
 	
 	grab_pivot = target_transform.affine_inverse() * to_pivot
-	transform = _world_grab.split_blend(transform, target_transform, .3, .1, .1, transform * grab_pivot, to_pivot)
-	#transform = target_transform
-	
-	
-	prev_hand_left_transform = hand_left.transform
-	prev_hand_right_transform = hand_right.transform
-	prev_hand_left_pressed = hand_left_pressed
-	prev_hand_right_pressed = hand_right_pressed
-	
-	pinch_timer -= _delta
-
-
-func pinch_transform(_transform: Transform3D, from_a: Vector3, from_b: Vector3, to_a: Vector3, to_b: Vector3) -> Transform3D:
-	from_b -= from_a
-	to_b -= to_a
-
-	_transform = _transform.translated(-from_a).scaled(Vector3(1, 1, 1) * sqrt(to_b.dot(to_b) / from_b.dot(from_b)))
-
-	var axis: Vector3 = from_b.cross(to_b)
-	var angle: float = from_b.angle_to(to_b)
-	if axis:
-		_transform = _transform.rotated(axis.normalized(), angle)
-
-	_transform = _transform.translated(to_a)
-
-	return _transform
+	transform = _world_grab.split_blend(transform, target_transform, .3, .1, .1, transform * target_transform.affine_inverse() * to_pivot, to_pivot)
+	#transform = target_transform'''
